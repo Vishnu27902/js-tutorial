98ba46e5506d51ee84c8fb40d6e711396878864f
Vishnu <vishnu27902@gmail.com>
2023-07-26 21:45:29 +0530

    Graph Implementation Successful

--- ds/graph.js
+++ ds/graph.js
@@ -36,15 +36,55 @@ class Graph {
     hasEdge(src, des) {
         return this.graph[src].includes(des)
     }
+    bfs(src) {
+        this.visited = []
+        this.queue = [src]
+        this.search(this.queue[0])
+        console.log(this.visited)
+    }
+    search(node) {
+        if (this.visited.includes(node)) {
+            return
+        }
+        this.visited.push(node)
+        this.queue.shift(node)
+        this.queue = [...new Set([...this.queue, ...this.graph[node]])]
+        for (let key of this.queue) {
+            if (!this.visited.includes(key)) {
+                this.search(key)
+            }
+        }
+    }
+    dfs(src) {
+        this.visited = []
+        this.dfsSearch(src)
+        console.log(this.visited)
+    }
+    dfsSearch(node) {
+        if (this.visited.includes(node)) {
+            return
+        }
+        this.visited.push(node)
+        for (let key of Object.keys(this.graph)) {
+            if (!this.visited.includes(key)) {
+                this.dfsSearch(parseInt(key))
+            }
+        }
+    }
 }
 
 const graph = new Graph();
 graph.addEdge(1, 2)
-graph.addEdge(1, 3)
 graph.addEdge(1, 4)
+graph.addEdge(1, 5)
+graph.addEdge(1, 3)
 graph.addEdge(2, 3)
 graph.addEdge(5, 6)
 graph.addEdge(3, 2)
 graph.addEdge(4, 5)
 
 console.log(graph.graph)
+console.log("Breath First Search")
+graph.bfs(1)
+console.log("Depth First Search")
+graph.dfs(1)
