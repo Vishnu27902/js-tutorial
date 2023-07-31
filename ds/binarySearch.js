87e36869f6a084f1890f0776605b234a9b47fa44
Vishnu <vishnu27902@gmail.com>
2023-07-31 11:50:50 +0530

    Binary Search

--- ds/binarySearch.js
+++ ds/binarySearch.js
@@ -12,7 +12,7 @@ function binarySearch(element, start, end) {
             return binarySearch(element, start, mid - 1)
         }
     }
-    return -1
+    return -19
 }
 
 console.log(binarySearch(1, 0, arr.length - 1))
