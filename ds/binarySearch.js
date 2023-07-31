b2755a38adc215c94dd988bb84b243789c17ec0e
Vishnu <vishnu27902@gmail.com>
2023-07-31 11:25:00 +0530

    Binary Search

---
+++ ds/binarySearch.js
@@ -0,0 +1,18 @@
+const arr = [1, 6, 2, 4, 9, 3, 5]
+arr.sort()
+
+function binarySearch(element, start, end) {
+    let mid = parseInt((end + start) / 2)
+    if (start <= end) {
+        if (arr[mid] == element) {
+            return mid
+        } if (arr[mid] < element) {
+            return binarySearch(element, mid + 1, end)
+        } if (arr[mid] > element) {
+            return binarySearch(element, start, mid - 1)
+        }
+    }
+    return -1
+}
+
+console.log(binarySearch(1, 0, arr.length - 1))
