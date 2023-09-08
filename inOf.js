7b1b01746720bf665ce9d6d108a30f04a4db8489
Vishnu <vishnu27902@gmail.com>
2023-09-08 21:55:56 +0530

    Update

---
+++ inOf.js
@@ -0,0 +1,16 @@
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// for (let i in a) {
+//     console.log(i)
+// }
+// for (let i of a) {
+//     console.log(i)
+// }
+const b = { "H": "Hello", "W": "Welcome", "T": "To", "N": "Node Js" }
+// for (let i of b) {
+//     console.log(i)
+// }
+// for (let i in b) {
+//     console.log(i)
+// }
+// a.forEach((ele,ind,arr)=>console.log(ele+" "+ind+" "+arr))
+console.log(typeof [])
--- sample.js
+++ sample.js
@@ -1,18 +1,20 @@
-const obj = {
-    name: "Vishnu",
-    age: 20,
-    greet: function () {
-        console.log("Hello " + this.name);
-    }
-}
+// const obj = {
+//     name: "Vishnu",
+//     age: 20,
+//     greet: function () {
+//         console.log("Hello " + this.name);
+//     }
+// }
 
-obj.greet()
+// obj.greet()
 
-function greeting() {
-    console.log("Welcome " + this.name)
-}
+// function greeting() {
+//     console.log("Welcome " + this.name)
+// }
 
-greeting.call(obj)
-greeting.apply(obj)
-const newGreeting = greeting.bind(obj)
-newGreeting()
+// greeting.call(obj)
+// greeting.apply(obj)
+// const newGreeting = greeting.bind(obj)
+// newGreeting()
+
+console.log(typeof {})
